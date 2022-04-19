088dc4ff0a72e3fb5deaab0029f62b053af08799
kevingrau <okevindaniel66@gmail.com>
2022-04-19 17:32:53 -0300

    Terceira alteração

--- index.php
+++ index.php
@@ -4,7 +4,88 @@
     <meta charset="UTF-8" />
     <title>Cadastro</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="funCss.css" />
+
+    <style>
+
+.content {
+    width: 300px;
+    min-height: 560px;
+    margin: 0px auto;
+    position: relative;
+}
+
+h1, h3 {
+    font-size: 20px;
+    color: #066a75;
+    padding: 2px 0 10px 0;
+    font-family: Arial, sans-serif;
+    font-weight: bold;
+    text-align: center;
+    padding-bottom: 15px;
+}
+
+h1:after, h3:after {
+    content: ' ';
+    display: block;
+    width: 100%;
+    height: 2px;
+    margin-top: 10px;
+    background: -webkit-linear-gradient(left, rgba(147, 184, 189, 0) 0%, rgba(147, 184, 189, 0.8) 20%, rgba(147, 184, 189, 1) 53%, rgba(147, 184, 189, 0.8) 79%, rgba(147, 184, 189, 0) 100%);
+    background: linear-gradient(left, rgba(147, 184, 189, 0) 0%, rgba(147, 184, 189, 0.8) 20%, rgba(147, 184, 189, 1) 53%, rgba(147, 184, 189, 0.8) 79%, rgba(147, 184, 189, 0) 100%);
+}
+
+.content p:first-child {
+    margin: 0px;
+}
+
+label {
+    color: #405c60;
+    position: relative;
+}
+
+input {
+    width: 300px;
+    margin-top: 4px;
+    padding: 5px;
+    border: 1px solid #b2b2b2;
+    -webkit-border-radius: 3px;
+    border-radius: 3px;
+    -webkit-box-shadow: 0px 1px 4px 0px rgba(168, 168, 168, 0.6) inset;
+}
+
+input[type="submit"] {
+    width: 300px;
+ 
+    background: rgb(61, 157, 179);
+    padding: 5px;
+    color: #fff;
+    font-size: 14px;
+    border: 1px solid #fff;
+    margin-bottom: 3px;
+    text-shadow: 0 1px 1px #333;
+    -webkit-border-radius: 5px;
+    border-radius: 5px;
+}
+
+#cadastro {
+    position: absolute;
+    top: 10px;
+    width: 105%;
+    padding: 18px;
+    margin: 0 0 35px 0;
+    background: #f7f7f7;
+    border: 1px solid rgba(147, 184, 189, 0.8);
+    border-radius: 5px;
+    -webkit-animation-duration: 0.5s;
+    -webkit-animation-timing-function: ease;
+    -webkit-animation-fill-mode: both;
+    animation-duration: 0.5s;
+    animation-timing-function: ease;
+    animation-fill-mode: both;
+}
+
+    </style>
+
 </head>
 
 <body>
@@ -15,34 +96,31 @@
 
 
             <div id="cadastro">
-                <form method="post" action="">
+                <form method="post" action="" class="forms">
                     <h1>Cadastro</h1>
 
                     <p>
                         <label for="nome">Nome</label>
                         <input id="nome" name="nome"type="text" />
-                    </p>
-
-                    <p>
+                    
                         <label for="cidade">Cidade</label>
                         <input id="cidade" name="cidade"type="text" />
-                    </p>
-
-                    <p>
+                    
                         <label for="salHora">Salário por hora</label>
-                        <input id="salHora" name="salHora"type="number" />
-                    </p>
+                        <input id="salHora" name="salHora"type="number" min="0" />
 
-                    <p>
                         <label for="horasTrab">Horas trabalhadas</label>
-                        <input id="horasTrab" name="horasTrab"type="number" />
-                    </p>
-                    <p>
-                        <input type="submit" class="btns" value="Cadastrar" name="btnCadastrar">
-                        <input type="submit" class="btns" value="Calcular Salário" name="btnCalcular">
-                        <input type="submit" class="btns" value="Mostrar Dados" name="btnMostrar">
-                        <input type="submit" class="btns" value="Aumento Salarial" name="btnAumentar">
-                    </p>
+                        <input id="horasTrab" name="horasTrab"type="number" min="0" />
+
+                        <label for="aumento">Aumento: </label><br>
+                         <input id="aumento" name="aumento" type="number" min="0"><br><br>
+                    </p> 
+
+                        <input type="submit" value="Cadastrar" name="btnCadastrar">
+                        <input type="submit" value="Calcular salário" name="btnCalcular">
+                        <input type="submit" value="Mostrar dados" name="btnMostrar">
+                        <input type="submit" value="Aumento salarial" name="btnAumentar">
+
                 </form>
             </div>
         </div>
@@ -52,10 +130,11 @@
 </html>
 
 <?php 
-    require_once "Funcionario.php";
+    require_once "funcionario.php";
     $funcionario = new Funcionario();
     
-    if(isset($_POST["btnCadastrar"]) && !empty($_POST['nome']))
+    if(isset($_POST["btnCadastrar"]) && !empty($_POST['nome']) && !empty($_POST['cidade']) && !empty($_POST['salHora'])
+    && !empty($_POST['horasTrab']))
     {
         $funcionario->setNome($_POST['nome']);
         $funcionario->setCidade($_POST['cidade']);
@@ -63,7 +142,7 @@
         $funcionario->setHorasTrab($_POST['horasTrab']);
         $funcionario->setSalarioFinal($funcionario->calcularSalario());
 
-        echo "<div class='container'><h3>Usuário Cadastrado com sucesso</h3><div>";
+        echo "<h3>Usuário Cadastrado com sucesso</h3>";
     }else if(isset($_POST["btnCadastrar"])){
         echo "Preencha o nome!";
     }
@@ -73,7 +152,7 @@
         $funcionario->setSalHora($_POST['salHora']);
         $funcionario->setHorasTrab($_POST['horasTrab']);
         $salario = $funcionario->calcularSalario();
-        echo "<div class='container'><h3>Salário do Funcionário: <span id='salarioColor'>R$ $salario</span></h3></div>";
+        echo "<h3>Salário do Funcionário: <span>R$ $salario</span></h3>";
     }
 
     if(isset($_POST["btnMostrar"]))
@@ -84,7 +163,7 @@
         $funcionario->setHorasTrab($_POST['horasTrab']);
         $funcionario->setSalarioFinal($funcionario->calcularSalario());
         $dados = $funcionario->mostrarDados();
-        echo "<div class='container'><h3>$dados<h3><div>";
+        echo "<h3>$dados<h3>";
     }
 
     if(isset($_POST["btnAumentar"]))
@@ -93,7 +172,7 @@
         $funcionario->setHorasTrab($_POST['horasTrab']);
         $funcionario->setSalarioFinal($funcionario->calcularSalario());
         $aumento = $funcionario->darAumento($_POST['aumento']);
-        echo "<div class='container'><h3>Salário com aumento: <span id='salarioColor'>R$ $aumento</span><h3><div>";
+        echo "<h3>Salário com aumento: <span>R$ $aumento</span><h3>";
     }
 
 ?>
